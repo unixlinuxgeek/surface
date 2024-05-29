25daf2ec875f0f02a99fea621569f6aced1c2c40
unixlinuxgeek <unixlinuxgeek@gmail.com>
2024-05-29 16:49:21 +0500

    Completed exercise 3.1

--- surface.go
+++ surface.go
@@ -22,30 +22,54 @@ func main() {
 		"width='%d' height='%d'>", width, height)
 	for i := 0; i < cells; i++ {
 		for j := 0; j < cells; j++ {
-			ax, ay := corner(i+1, j)
-			bx, by := corner(i, j)
-			cx, cy := corner(i, j+1)
-			dx, dy := corner(i+1, j+1)
+			ax, ay, err := corner(i+1, j)
+			if err != nil {
+				break
+			}
+			bx, by, err := corner(i, j)
+			if err != nil {
+				break
+			}
+			cx, cy, err := corner(i, j+1)
+			if err != nil {
+				break
+			}
 
+			dx, dy, err := corner(i+1, j+1)
+			if err != nil {
+				break
+			}
 			fmt.Printf("<polygon points='%g, %g, %g, %g, %g, %g, %g, %g'/>\n", ax, ay, bx, by, cx, cy, dx, dy)
 		}
 	}
 	fmt.Println("</svg>")
 }
 
-func corner(i, j int) (float64, float64) {
+func corner(i, j int) (float64, float64, error) {
 	// Ищем угловую точку (x, y ) ячейки (i, j).
 	x := xyrange * (float64(i)/cells - 0.5)
 	y := xyrange * (float64(j)/cells - 0.5)
 	// Вычисляем высоту поверхноости z
-	z := f(x, y)
+	z, err := f(x, y)
+	if err != nil {
+		//log.Fatalf("%s is Infinity Error!!!", err)
+		return 0, 0, err
+	}
 	// Изометрически проецируем (x, y, z) на двумерную канву SVG (sx, sy)
 	sx := width/2 + (x+y)*cos30*xyscale
 	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
-	return sx, sy
+	return sx, sy, nil
 }
 
-func f(x, y float64) float64 {
+func f(x, y float64) (float64, error) {
 	r := math.Hypot(x, y) // Расстояние от (0,0)
-	return math.Sin(r) / r
+	c := math.Sin(r) / r
+
+	// IsInf сообщает, является ли f бесконечностью в соответствии со знаком.
+	// Если знак == 0, IsInf сообщает, является ли f бесконечностью. <---
+	if math.IsInf(c, 0) == false {
+		return math.Round(math.Sin(r) / r), nil
+	} else {
+		return 0, fmt.Errorf("%s", "Error!!!")
+	}
 }
